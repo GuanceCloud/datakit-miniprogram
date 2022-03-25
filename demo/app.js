4b0f6fa11ea5e6115e5576ba67a33123be6d11b6
邓昌亮 <arnolddeng@126.com>
2022-03-25 14:10:02 +0800

    修改版本

--- demo/app.js
+++ demo/app.js
@@ -1,7 +1,7 @@
 // //app.js
-const { datafluxRum } = require('./miniprogram/dataflux-rum-miniapp')
+// const { datafluxRum } = require('./miniprogram/dataflux-rum-miniapp')
 // // 初始化 Sentry
-// const { datafluxRum } = require('@cloudcare/rum-miniapp')
+const { datafluxRum } = require('@cloudcare/rum-miniapp')
 
 datafluxRum.init({
 	datakitOrigin: 'http://172.16.2.201:31845',
@@ -9,19 +9,19 @@ datafluxRum.init({
 	env: 'prod',
 	version: '1.0.0',
 	trackInteractions: true,
-	allowedTracingOrigins: ['http://testing-ft2x-api.cloudcare.cn'],
-	traceType: 'zipkin'
+	// allowedTracingOrigins: ['http://testing-ft2x-api.cloudcare.cn'],
+	// traceType: 'zipkin'
 })
-datafluxRum.setUser({
-	id: '1234',
-	name: 'John Doe',
-	email: 'john@doe.com',
-})
-datafluxRum && datafluxRum.addRumGlobalContext('isvip', 'xxxx');                     
-datafluxRum.addRumGlobalContext('activity', {
-    hasPaid: true,
-    amount: 23.42
-});
+// datafluxRum.setUser({
+// 	id: '1234',
+// 	name: 'John Doe',
+// 	email: 'john@doe.com',
+// })
+// datafluxRum && datafluxRum.addRumGlobalContext('isvip', 'xxxx');                     
+// datafluxRum.addRumGlobalContext('activity', {
+//     hasPaid: true,
+//     amount: 23.42
+// });
 // const { datafluxRum } = require('@cloudcare/rum-miniapp')
 // // // 初始化 Sentry
 // datafluxRum.init({
